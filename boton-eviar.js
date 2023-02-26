87769026942a2166e8bce591fa632bdf951f309b
cristianT97 <126366685+cristianT97@users.noreply.github.com>
2023-02-25 23:12:54 -0300

    Add files via upload

---
+++ boton-eviar.js
@@ -0,0 +1,7 @@
+// Selecciona el botón por su ID
+var btnHeader = document.getElementById("btn-header");
+
+// Agrega un evento de clic al botón
+btnHeader.addEventListener("click", function() {
+  alert("EL CURRICULUM YA FUE ENVIADO");
+});
